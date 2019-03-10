ac9cf9bd56cd6ab4e152859fa7480753df053afd
J. David Giese <johndgiese@gmail.com>
2019-03-09 21:03:27 -0500

    Finish up basic version

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./index.scss";
 
 const QUESTIONS_PER_QUIZ = 20;
 
-const BOOKS = ['Genesis', 'Exodous', 'Leviticus', 'Numbers', 'Deuteronomy'];
+const BOOKS = ['Genesis', 'Exodus', 'Leviticus', 'Numbers', 'Deuteronomy'];
 
 const CHAPTERS_PER_BOOK = [50, 40, 27, 36, 34];
 
@@ -17,12 +17,18 @@ function getRandomVerse() {
 }
 
 function verseToLabel(verseArray) {
-    const [bookNum, chapter, verse] = verseArray;
-    return bookNumToLabel(bookNum) + ' ' + chapter + '.' + verse;
+    let bookNum, chapter, verse;
+    if (verseArray.length === 3) {
+        [bookNum, chapter, verse] = verseArray;
+        return bookNumToLabel(bookNum) + ' ' + chapter + '.' + verse;
+    } else if (verseArray.length === 2) {
+        [bookNum, chapter] = verseArray;
+        return bookNumToLabel(bookNum) + ' ' + chapter;
+    }
 }
 
 function bookNumToLabel(bookNum) {
-    return BOOKS[bookNum + 1];
+    return BOOKS[bookNum - 1];
 }
 
 function getRandomInt(max) {
@@ -110,18 +116,21 @@ class App extends React.Component {
     render() {
         const {score, questionNum, verse, bookChoice, chapterChoice} = this.state;
         let onClick;
+        let showAnswer;
         if (bookChoice === null) {
             onClick = this.pickBook;
+            showAnswer = false;
         } else if (chapterChoice === null) {
             onClick = this.pickChapter;
+            showAnswer = false;
         } else {
             onClick = this.nextQuestion;
+            showAnswer = true;
         }
-        const verseText = getVerseText(verse);
         return (
             <div className="app">
                 <Header score={score} questionNum={questionNum} questionsPerQuiz={QUESTIONS_PER_QUIZ} />
-                <Body>{verseText}</Body>
+                <Body showAnswer={showAnswer} verse={verse} bookChoice={bookChoice} chapterChoice={chapterChoice} />
                 <Footer bookChoice={bookChoice} chapterChoice={chapterChoice} onClick={onClick} />
             </div>
         );
@@ -140,43 +149,66 @@ function Header ({score, questionNum, questionsPerQuiz}) {
 }
 
 
-function Body ({children}) {
-    return (
-        <div className="body">
-            <p>{children}</p>
-        </div>
-    );
-}
-
-function Footer ({bookChoice, chapterChoice, onClick}) {
-    if (bookChoice === null) {
+function Body ({showAnswer, verse, bookChoice, chapterChoice}) {
+    const verseText = getVerseText(verse);
+    if (showAnswer) {
+        // TODO: show nearby verses with answer
+        let resultLabel;
+        if (verse[0] === bookChoice && verse[1] === chapterChoice) {
+            resultLabel = <p className="body__correct">CORRECT, +30 points!</p>;
+        } else if (verse[0] === bookChoice) {
+            resultLabel = <p className="body__close">Close, you chose {verseToLabel([bookChoice, chapterChoice])}! +10 points for getting the book correct</p>;
+        } else {
+            resultLabel = <p className="body__wrong">Wrong, you chose {verseToLabel([bookChoice, chapterChoice])}</p>;
+        }
         return (
-            <div className="footer">
-                <Btn onClick={() => onClick(1)}>Genesis</Btn>
-                <Btn onClick={() => onClick(2)}>Exodus</Btn>
-                <Btn onClick={() => onClick(3)}>Leviticus</Btn>
-                <Btn onClick={() => onClick(4)}>Numbers</Btn>
-                <Btn onClick={() => onClick(5)}>Deut.</Btn>
-            </div>
-        );
-    } else if (chapterChoice === null) {
-        const numChapters = CHAPTERS_PER_BOOK[bookChoice - 1];
-        const chapters = Array.apply(null, {length: numChapters}).map(Number.call, Number);
-        return (
-            <div className="footer">{chapters.map(i => <Btn key={i} onClick={() => onClick(i + 1)}>{i + 1}</Btn>)}
+            <div className="body">
+                <h2>Answer: {verseToLabel(verse)}</h2>
+                {resultLabel}
+                <p>{verseText}</p>
             </div>
         );
     } else {
         return (
-            <div className="footer">
-                <Btn onClick={() => onClick()}>Next Question</Btn>
+            <div className="body">
+                <p>{verseText}</p>
             </div>
         );
     }
 }
 
-function Btn ({children, onClick}) {
-    return <div className="button" onClick={onClick}>{children}</div>
+const BOOK_COLORS = ['#EC6B34', '#EAC74B', '#55AEEE', '#AE9BFB', '#F59A2F'];
+
+function Footer ({bookChoice, chapterChoice, onClick}) {
+    if (bookChoice === null) {
+        return (
+            <div className="footer">
+                <Btn color={BOOK_COLORS[0]} onClick={() => onClick(1)}>Genesis</Btn>
+                <Btn color={BOOK_COLORS[1]} onClick={() => onClick(2)}>Exodus</Btn>
+                <Btn color={BOOK_COLORS[2]} onClick={() => onClick(3)}>Leviticus</Btn>
+                <Btn color={BOOK_COLORS[3]} onClick={() => onClick(4)}>Numbers</Btn>
+                <Btn color={BOOK_COLORS[4]} onClick={() => onClick(5)}>Deut.</Btn>
+            </div>
+        );
+    } else if (chapterChoice === null) {
+        const numChapters = CHAPTERS_PER_BOOK[bookChoice - 1];
+        const chapters = Array.apply(null, {length: numChapters}).map(Number.call, Number);
+        const color = BOOK_COLORS[bookChoice - 1];
+        return (
+            <div className="footer">{chapters.map(i => <Btn color={color} key={i} onClick={() => onClick(i + 1)}>{i + 1}</Btn>)}
+            </div>
+        );
+    } else {
+        return (
+            <div className="footer">
+                <Btn color={'#5D4DC3'} onClick={() => onClick()}>Next Question</Btn>
+            </div>
+        );
+    }
+}
+
+function Btn ({children, onClick, color}) {
+    return <div style={{backgroundColor: color}} className="button" onClick={onClick}>{children}</div>
 }
 
 ReactDOM.render(
